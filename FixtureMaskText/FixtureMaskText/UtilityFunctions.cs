ea7212022880218b7cf1b62c8146e6018ee53d61
Peter N K <pnk@avb.bg>
2018-06-08 10:32:10 +0300

    Ready for use. The test was created in version 2.9.14 The test shoud stat only if the FixtureMask button is on focus.

---
+++ FixtureMaskText/FixtureMaskText/UtilityFunctions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FixtureMaskText
+{
+    class ReportText
+    {
+        public static void RunPositiveReport(string ActualText)
+        {
+            Ranorex.Report.Success("Sucessfully performed: ", ActualText);
+           
+        }
+
+        public static void RunNegativeReport(string ExpectedText, string ActualText)
+        {
+            Ranorex.Report.Error("STOP THE VERSION: ", ActualText);
+            Ranorex.Report.Error("THE TEXT MUST BE: ", ExpectedText);
+        }
+    }
+
+
+
+    class UtilityRun
+    {
+        public static void CompareTitleText(string Actual, string Hardcoded)
+        {
+            string ActualFixtureTitleText = Actual;
+            string HardcodedFixtureTitleText = Hardcoded;
+            bool bTitleText = true;
+
+            bTitleText = ActualFixtureTitleText.CompareTo(HardcodedFixtureTitleText) == 0;
+
+            if (bTitleText)
+            {
+                ReportText.RunPositiveReport(ActualFixtureTitleText);
+            }
+            else
+            {
+                ReportText.RunNegativeReport(ActualFixtureTitleText, HardcodedFixtureTitleText);
+            }
+        }
+    }
+}
